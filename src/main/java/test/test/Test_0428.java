140995cccece2c2884af63497fb99f5a468137ae
wuxia5 <wuxia5@xiaomi.com>
2022-06-01 15:37:33 +0800

    力扣 04-28

--- src/main/java/test/test/Test_0428.java
+++ src/main/java/test/test/Test_0428.java
@@ -1,7 +1,10 @@
 package test.test;
 
 import java.util.*;
-
+/**
+ * 重点整理的题目：
+ * 409 416 621 647
+ */
 public class Test_0428 {
     //409
     public int longestPalindrome(String s) {
@@ -11,62 +14,26 @@ public class Test_0428 {
         }
         //累加偶数
         int res = 0;
-        //最大的奇数
-        int maxOdd = 0;
 
         for (Map.Entry<Character, Integer> entry: map.entrySet()) {
-            //偶数直接加
-            if (entry.getValue() % 2 == 0) {
-                res += entry.getValue();
-            } else {
-                //奇数取最大
-                maxOdd = Math.max(maxOdd, entry.getValue());
+            //偶数全加上，奇数加entry.getValue()/2 * 2个
+            res += entry.getValue()/2 * 2;
+
+            //如果出现过奇数，加一次，作为回文的核心
+            if (entry.getValue() % 2  == 1 && res % 2 == 0) {
+                res ++;
             }
         }
 
-        return res + maxOdd;
+        return res ;
     }
 
-    //416
+    //416 todo 动态规划算法优化解法二
     public boolean canPartition(int[] nums) {
         int n = nums.length;
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-        }
-        //如果数组的和为奇数
-        if (sum % 2 != 0) {
+        if (n <= 1) {
             return false;
         }
-        sum = sum / 2;
-        //dp[i] 表示是否使用nums[i];
-        int[] dp = new int[n];
-        boolean search = search(nums, dp, sum);
-        return search;
-    }
-
-    public boolean search(int[] nums,int[] dp, int target) {
-        if (target == 0) {
-            return true;
-        }
-
-        for (int i = 0; i< nums.length; i++) {
-            if (dp[i] == 0 && target >= nums[i]) {
-                dp[i] = 1;
-                boolean succ = search(nums, dp, target - nums[i]);
-                if (succ) {
-                    return succ;
-                }
-                //reset
-                dp[i] = 0;
-            }
-        }
-        return false;
-    }
-
-    //416 动态规划算法优化解法二
-    public boolean canPartition2(int[] nums) {
-        int n = nums.length;
         int sum = 0;
         for (int i = 0; i < n; i++) {
             sum += nums[i];
@@ -78,8 +45,11 @@ public class Test_0428 {
         sum = sum / 2;
         //dp[i] 表示使用nums[0...i]是否可以凑成和为j的数;
         boolean [][] dp = new boolean [n][sum + 1];
-        dp[0][nums[0]] = true;
-        for (int i = 0; i< n; i++) {
+        //[9,5]
+        if (nums[0] <= sum) {
+            dp[0][nums[0]] = true;
+        }
+        for (int i = 0; i < n; i++) {
             dp[i][0] = true;
         }
         for (int i = 1; i< n; i++) {
@@ -88,7 +58,7 @@ public class Test_0428 {
                 dp[i][j] = dp[i-1][j];
                 //可以选择nums[i]
                 if ( j >= nums[i]) {
-                    dp[i][j] = dp[i][j] || dp[i][j-nums[i]];
+                    dp[i][j] = dp[i][j] | dp[i-1][j-nums[i]];
                 }
             }
         }
@@ -99,6 +69,9 @@ public class Test_0428 {
     //416 动态规划空间优化解法三
     public boolean canPartition3(int[] nums) {
         int n = nums.length;
+        if (n <= 1) {
+            return false;
+        }
         int sum = 0;
         for (int i = 0; i < n; i++) {
             sum += nums[i];
@@ -111,13 +84,11 @@ public class Test_0428 {
         //dp[i] 表示使用nums[0...i]是否可以凑成和为j的数;
         boolean [] dp = new boolean [sum + 1];
         dp[0] = true;
-        dp[nums[0]] = true;
-        for (int i = 1; i< n; i++) {
-            for (int j = 1; j <= sum; j++) {
-                //可以选择nums[i]
-                if ( j >= nums[i]) {
-                    dp[j] = dp[j] || dp[j-nums[i]];
-                }
+        for (int i = 0; i< n; i++) {
+            //从后往前覆盖(从前往后可能会一个数字使用多次)
+            for (int j = sum; j >= nums[i]; j--) {
+                dp[j] = dp[j] | dp[j-nums[i]];
+
             }
         }
         return dp[sum];
@@ -165,6 +136,9 @@ public class Test_0428 {
 
     //565
     public int arrayNesting(int[] nums) {
+        if (nums.length == 1) {
+            return 1;
+        }
         int res = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i]> 0) {
@@ -187,10 +161,7 @@ public class Test_0428 {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
         int m = mat.length;
         int n = mat[0].length;
-        if (m * n != r * c) {
-            return null;
-        }
-        if (m == r && n == c) {
+        if ((m * n != r * c) || (m == r && n == c)) {
             return mat;
         }
 
@@ -225,36 +196,25 @@ public class Test_0428 {
         /**
          * dp[i][j] 表示将word1[0..i]与word2[0..j]变的相同所需的最少步骤
          */
-        int[][]dp = new int[m][n];
-        dp[0][0] = word1.charAt(0) == word2.charAt(0)? 0:2;
+        int[][]dp = new int[m+1][n+1];
         //初始化第一行
-        for (int j = 1; j< n; j++) {
-            //能通过dp[0][j-1] + 1转化或者全删除(j+2)从而达到一致
-            dp[0][j] = Math.min(dp[0][j-1] + 1, j + 2);
-            //如果比较的字符相等
-            if (word1.charAt(0) == word2.charAt(j)) {
-                dp[0][j] = Math.min(dp[0][j],j);
-            }
+        for (int j = 1; j<= n; j++) {
+            dp[0][j] = j;
         }
         //初始化第一列
-        for (int i = 1; i< m; i++) {
-            //能通过dp[i][0] + 1转化或者全删除(i+2)从而达到一致
-            dp[i][0] = Math.min(dp[i-1][0] + 1, i + 2);
-            //如果比较的字符相等
-            if (word1.charAt(i) == word2.charAt(0)) {
-                dp[i][0] = Math.min(dp[i][0],i);
-            }
+        for (int i = 1; i<= m; i++) {
+            dp[i][0] = i;
         }
 
-        for (int i = 1; i< m; i++) {
-            for (int j = 1; j< n; j++) {
+        for (int i = 1; i<= m; i++) {
+            for (int j = 1; j<= n; j++) {
                 dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) +1;
-                if (word1.charAt(i) == word2.charAt(j)) {
+                if (word1.charAt(i-1) == word2.charAt(j-1)) {
                     dp[i][j] = Math.min(dp[i][j], dp[i-1][j-1]);
                 }
             }
         }
-        return dp[m-1][n-1];
+        return dp[m][n];
     }
 
 
@@ -292,7 +252,7 @@ public class Test_0428 {
         return max;
     }
 
-    //645 todo test
+    //645
     public int[] findErrorNums(int[] nums) {
         int[] res = new int[2];
         for (int i = 0; i< nums.length; i++) {
@@ -315,34 +275,116 @@ public class Test_0428 {
         return res;
     }
 
-    //647 todo test
+    /**
+     * todo [重要]5 Manacher 算法求解最长回文子串的长度
+     * @param s
+     * @return
+     */
+    public String longestPalindrome2(String s) {
+        int start = 0, end = -1;
+        StringBuffer t = new StringBuffer("#");
+        for (int i = 0; i < s.length(); ++i) {
+            t.append(s.charAt(i));
+            t.append('#');
+        }
+        s = t.toString();
+
+        List<Integer> arm_len = new ArrayList<Integer>();
+        int right = -1, j = -1;
+        for (int i = 0; i < s.length(); ++i) {
+            int cur_arm_len;
+            if (right >= i) {
+                //找到 i 关于中心 j 的对称点i_sym
+                int i_sym = j * 2 - i;
+                // 计算不用重复计算的长度。小于 right - i 是为了保证 不重复计算的范围在 j 的有效范围内
+                int min_arm_len = Math.min(arm_len.get(i_sym), right - i);
+                //从 i - min_arm_len 开始扩展比较是否是回文子串,返回当前中心的最长臂长
+                cur_arm_len = expand(s, i - min_arm_len, i + min_arm_len);
+            } else {
+                cur_arm_len = expand(s, i, i);
+            }
+            //arm_len[i] = 以 i 为中心的臂长。由于 i 是递增的，所以直接往list 里面添加值即可
+            arm_len.add(cur_arm_len);
+            //扩展后的最右边界更新
+            if (i + cur_arm_len > right) {
+                j = i;
+                right = i + cur_arm_len;
+            }
+            //扩展后的臂长更新,更新最长回文子串的起始位置
+            if (cur_arm_len * 2 + 1 > end - start) {
+                start = i - cur_arm_len;
+                end = i + cur_arm_len;
+            }
+        }
+
+        StringBuffer ans = new StringBuffer();
+        for (int i = start; i <= end; ++i) {
+            if (s.charAt(i) != '#') {
+                ans.append(s.charAt(i));
+            }
+        }
+        return ans.toString();
+    }
+
+    public int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            --left;
+            ++right;
+        }
+        return (right - left - 2) / 2;
+    }
+
+
+    /**
+     * 647
+     * Manacher 算法
+     * todo 需要注意的是不能让下标越界，有一个很简单的办法，就是在开头加一个 \$$，并在结尾加一个 !!，这样开头和结尾的两个字符一定不相等，循环就可以在这里终止。
+     * @param s
+     * @return
+     */
     public int countSubstrings(String s) {
         int n = s.length();
-        //dp[i]表示s[0..i]的子串的 回文子串 的数目
-        int[] dp = new int[n];
-        dp[0] = 1;
-        for (int i = 1; i< n; i++) {
-            //dp[i]的回文子串包含dp[i-1]的回文子串
-            dp[i] = dp[i-1];
-            int cnt = 0;
-            //s[i]与s[j]构成回文子串的数量有cnt个
-            for (int j = 0; j < i; j++) {
-                if (s.charAt(j) == s.charAt(i)) {
-                    cnt++;
-                }
-            }
-            //加1代表s.charAt(i)构成回文子串
-            dp[i] += cnt + 1;
+        StringBuffer t = new StringBuffer("$#");
+        for (int i = 0; i < n; ++i) {
+            t.append(s.charAt(i));
+            t.append('#');
         }
-        return dp[n-1];
+        n = t.length();
+        t.append("!");
+        //f[i] 表示以 t[i]为中心的最长臂长(包含#)
+        int[] f = new int[n];
+        /**
+         * iMax : 中心
+         * rMax : 以 iMax为中心的右边界
+         */
+        int iMax = 0, rMax = 0, ans = 0;
+        for (int i = 1; i < n; ++i) {
+            // 初始化 f[i]
+            f[i] = i <= rMax ? Math.min(rMax - i + 1, f[2 * iMax - i]) : 1;
+            // 中心拓展
+            while (t.charAt(i + f[i]) == t.charAt(i - f[i])) {
+                ++f[i];
+            }
+            // 动态维护 iMax 和 rMax
+            if (i + f[i] - 1 > rMax) {
+                iMax = i;
+                rMax = i + f[i] - 1;
+            }
+            // 统计答案, 当前贡献为 f[i] / 2 上取整(因为 f[i] 计算的臂长包含填充字符，所以要/2)
+            ans += f[i] / 2;
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
         Test_0428 test_0428 = new Test_0428();
-        //String s = "bb";
+        String s = "abccccdd";
         //int i = test_0428.longestPalindrome(s);
-        //int[] p = {5,4,0,3,1,6,2};
-        //test_0428.canPartition(p);
+        int[] p = {2,2,3,5};
+        //int i = test_0428.minDistance("a", "b");
+        int abc = test_0428.countSubstrings("abc");
+        //boolean b = test_0428.canPartition3(p);
         //boolean b = test_0428.canPartition2(p);
         //boolean b = test_0428.canPartition3(p);
         //List<Integer> duplicates = test_0428.findDuplicates(p);
@@ -358,10 +400,10 @@ public class Test_0428 {
         char[] task = {'A','A','A','B','B','B'};
         int n = 2;
         //int i = test_0428.leastInterval(task, n);
-        int[] p = {1,1};
+        //int[] p = {1,1};
         //int[] errorNums = test_0428.findErrorNums(p);
-        String s = "aaa";
-        int i = test_0428.countSubstrings(s);
+        //String s = "aaa";
+        //int i = test_0428.countSubstrings(s);
         System.out.println();
     }
 }
